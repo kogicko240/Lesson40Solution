ca47fc0d02effc311ac5ff189fd4517c36444818
kogicko240 <dmn120112@gmail.com>
2026-02-11 17:11:36 +0300

    Add project files.

---
+++ FunctionRevisionProject/task03.cpp
@@ -0,0 +1,14 @@
+﻿// Задание 3: Вызовите функцию `print_сhar_and_string` в `main`.
+
+#include <iostream>
+using namespace std;
+
+... print_сhar_and_string(... symbol, ... text)
+{
+    cout << symbol << ": " << text;
+}
+
+int main()
+{
+    return 0;
+}
---
+++ FunctionRevisionProject/task04.cpp
@@ -0,0 +1,16 @@
+﻿// Задание 4: Добавьте прототип функции, которая принимает булевское 
+// и целочисленное значения и возвращает истину, если число чётное.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << is_even(true, 4);
+    return 0;
+}
+
+... is_even(... flag, ... num)
+{
+    return flag && (num % 2 == 0);
+}
